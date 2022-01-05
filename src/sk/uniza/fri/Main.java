91fc37880834c6de2abcac24ab1154fb7aca816a
Andrej <kopko158@azet.sk>
2022-01-05 11:44:18 +0100

    Zaklad

---
+++ src/sk/uniza/fri/Main.java
@@ -0,0 +1,14 @@
+package sk.uniza.fri;
+/**
+ * Created by IntelliJ IDEA.
+ * User: Andrej
+ * Date: 5.1.2022
+ * Time: 11:43
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello world");
+    }
+}
